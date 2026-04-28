26514b56fa8dc72f8b81dcfd866e2d6fde8dc31f
kritesh_shah <kritesh.shah@9series.com>
2026-04-28 10:55:07 +0530

    add command.sh

--- command.sh
+++ command.sh
@@ -4,7 +4,6 @@ set -e
 
 #  python manage.py loaddata permissions asset_categories fields roles &&
 
-python manage.py migrate &&
 python manage.py collectstatic --noinput &&
 
 daphne -b 0.0.0.0 -p $PORT chatproject.asgi:application
