676e651d21e8fd1cbdd394038ebb155de8bf4d8f
kritesh_shah <kritesh.shah@9series.com>
2026-04-28 10:58:12 +0530

    add command.sh

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+
+from django.contrib import staticfiles
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +77,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ✅ Paste your OpenWeatherMap API key here
